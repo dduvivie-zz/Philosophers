3e6f638d8e4f107b93ce4a6b260017157d490936
Danae Duvivier <Danae.Duvivier@ulb.ac.be>
2022-10-12 12:20:31 +0200

    check arguments

--- check_args.c
+++ check_args.c
@@ -1,16 +1,19 @@
 #include "philosophers.h"
 
-/* Check if the argument contains only digits */
-int	arg_is_positive_num(char *arg)
+/* Check if the argument is integer */
+int	arg_is_int(char *arg)
 {
-	int	i;
+	long int	num;
 
-	i = 0;
-	while (arg[i] != '\0')
+	num = 0;
+	while (*arg != '\0')
 	{
-		if (!ft_isdigit(arg[i]))
+		if (!ft_isdigit(*arg))
 			return (0);
-		i++;
+		num = (num * 10) + (*arg - 48);
+		if (num > INT_MAX)
+			return (0);
+		arg++;
 	}
 	return (1);
 }
@@ -21,7 +24,7 @@ long	str_to_num(const char *str)
 	long int	num;
 
 	num = 0;
-	while (ft_isdigit(*str))
+	while (*str != '\0')
 	{
 		num = (num * 10) + (*str - 48);
 		str++;
@@ -32,48 +35,42 @@ long	str_to_num(const char *str)
 /* Check if the arguments numbers are correct */
 int	args_are_correct(int argc, char *argv[])
 {
-	int			i;
-	long int	num;
+	int	i;
+	int	num;
 
 	if (argc != 4 && argc != 5)
 		return (0);
 	i = 1;
-	while (i < argc)
+	while (i <= argc)
 	{
+		if (!arg_is_int(argv[i]))
+			return (0);
 		if (i == 1)
 		{
-			num = str_to_num(argv[i]);
-			if (num < 2)
+			num = (int)str_to_num(argv[i]);
+			if (num < 1)
 				return (0);
 		}
-		if (!arg_is_positive_num(argv[i]))
-			return (0);
 		i++;
 	}
 	return (1);
 }
 
-void	get_args_in_int(int argc, char *argv[], t_args *args)
+/* Get all arguments (string) to int */
+int	get_args_in_int(int argc, char *argv[], t_args *args)
 {
-	long int	num;
-
-	args->error_flag = 1;
 	if (!args_are_correct(argc, argv))
-		return ;
-	args->error_flag = 0;
-	num = str_to_num(argv[1]);
-	args->num_of_philo = (unsigned int)num;
-	num = str_to_num(argv[2]);
-	args->time_to_eat = (unsigned int)num;
-	num = str_to_num(argv[3]);
-	args->time_to_sleep = (unsigned int)num;
-	num = str_to_num(argv[4]);
-	args->time_to_think = (unsigned int)num;
-	if (argc == 5)
 	{
-		num = str_to_num(argv[5]);
-		args->num_of_times_eat = (int)num;
+		printf("Error: the arguments are invalid.\n");
+		return (0);
 	}
+	args->num_of_philo = (int)str_to_num(argv[1]);
+	args->time_to_die = (int)str_to_num(argv[2]);
+	args->time_to_eat = (int)str_to_num(argv[3]);
+	args->time_to_sleep = (int)str_to_num(argv[4]);
+	if (argc == 5)
+		args->num_of_times_eat = (int)str_to_num(argv[5]);
 	else
 		args->num_of_times_eat = -1;
+	return (1);
 }

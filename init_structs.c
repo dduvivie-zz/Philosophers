c469a0cf8e1437e120f372ea777589796522babc
Danae Duvivier <Danae.Duvivier@ulb.ac.be>
2022-10-12 14:09:06 +0200

    init structs 1

--- init_structs.c
+++ init_structs.c
@@ -1,73 +1,55 @@
 #include "philosophers.h"
 
+/* Init the list of threads. */
 pthread_t	*init_threads(t_args *args)
 {
-	pthread_t	*thread_list;
+	pthread_t	*threads;
 
-	thread_list = (pthread_t *)malloc(args->num_of_philo * sizeof(pthread_t));
-	if (!thread_list)
+	threads = (pthread_t *)malloc(args->num_of_philo * sizeof(pthread_t));
+	if (!threads)
 		return (NULL);
-	return (thread_list);
+	return (threads);
 }
 
-pthread_mutex_t	init_mutex(void)
+/* Init the end_flag variables and its mutex */
+int	init_endflag(t_args *args, t_endflag *end_flag)
 {
-	pthread_mutex_t	mutex;
-
-	pthread_mutex_init(&mutex, NULL);
-	return (mutex);
-}
-
-t_fork	*init_forks(t_args *args)
-{
-	unsigned int	i;
-	t_fork			*fork_list;
+	int	i;
 
 	i = 0;
-	fork_list = (t_fork *)malloc(args->num_of_philo * sizeof(t_fork));
-	if (!fork_list)
-		return (NULL);
+	end_flag->finish = 0;
+	end_flag->eat_counter = (int *)malloc(args->num_of_philo * sizeof(int));
+	if (!end_flag->eat_counter)
+		return (0);
 	while (i < args->num_of_philo)
 	{
-		fork_list[i].mutex = init_mutex();
-		fork_list[i].used = i + 1;
+		end_flag->eat_counter[i] = args->num_of_times_eat;
 		i++;
 	}
-	return (fork_list);
-}
-
-void	set_forks(t_philo *philo, int i, int last, t_fork *fork_list)
-{
-	if (i == 0)
-		philo->left_fork = &(fork_list[last]);
-	else
-		philo->left_fork = &(fork_list[i - 1]);
-	if (i == last)
-		philo->right_fork = &(fork_list[last]);
-	else
-		philo->right_fork = &(fork_list[i]);
-}
-
-t_philo	*init_philos(t_args *args, t_fork *fork_list)
-{
-	unsigned int	i;
-	unsigned int	last;
-	t_philo			*philo_list;
-
-	i = 0;
-	last = args->num_of_philo - 1;
-	philo_list = (t_philo *)malloc(args->num_of_philo * sizeof(t_philo));
-	if (!philo_list)
-		return (NULL);
-	while (i < args->num_of_philo)
+	if (pthread_mutex_init(&(end_flag->mutex), NULL) != 0)
 	{
-		philo_list[i].id = i + 1;
-		philo_list[i].time_to_eat = args->time_to_eat;
-		philo_list[i].time_to_sleep = args->time_to_sleep;
-		philo_list[i].time_to_think = args->time_to_think;
-		philo_list[i].num_of_times_eat = args->num_of_times_eat;
-		set_forks(&(philo_list[i]), (int)i, (int)last, fork_list);
-		i++;
+		free(end_flag->eat_counter);
+		return (0);
 	}
-	return (philo_list);
+	return (1);
+}
+
+/*
+Init all structures (t_thread, t_forks, t_endflag, t_philo)
+If it encount an error during malloc or init_mutex, call error_exit function.
+*/
+int	init_structs(t_program *prg)
+{
+	prg->threads = init_threads(&(prg->args));
+	if (!prg->threads)
+		return (0);
+	prg->forks = init_forks(&(prg->args));
+	if (!prg->forks)
+		return (error_exit(prg, 0));
+	if (!init_endflag(&(prg->args), &(prg->end_flag)))
+		return (error_exit(prg, 1));
+	prg->philos = init_philos(&(prg->args), prg->forks, &(prg->end_flag));
+	if (!prg->philos)
+		return (error_exit(prg, 2));
+	return (1);
 }

7767a82eac1c873635a4bbe533c757c5c0b3708d
Danae Duvivier <dduvivie@fu-r5-p5.s19.be>
2022-10-14 16:42:40 +0200

    output, check philo's life

--- philo_state.c
+++ philo_state.c
@@ -1,5 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_state.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dduvivie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/14 16:17:22 by dduvivie          #+#    #+#             */
+/*   Updated: 2022/10/14 16:17:24 by dduvivie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philosophers.h"
 
+/* Print the action of philosopher */
 void	print_message(long int time, t_philo *philo, char *msg)
 {
 	pthread_mutex_lock(&(philo->end_flag->mutex));
@@ -8,6 +21,7 @@ void	print_message(long int time, t_philo *philo, char *msg)
 	pthread_mutex_unlock(&(philo->end_flag->mutex));
 }
 
+/* Decrement the number of times philosopher must eat */
 void	count_eat(t_philo *philo)
 {
 	int	i;
@@ -19,47 +33,8 @@ void	count_eat(t_philo *philo)
 	pthread_mutex_unlock(&(philo->end_flag->mutex));
 }
 
-void	check_end(struct timeval *start, t_philo *philo, int *check_flag)
-{
-	int	i;
-	int	philo_num;
-
-	if (*check_flag == 0)
-	{
-		time_check(start, philo);
-		return ;
-	}
-	i = 0;
-	philo_num = philo->end_flag->philo_num;
-	pthread_mutex_lock(&(philo->end_flag->mutex));
-	while (i < philo_num)
-	{
-		if (philo->end_flag->eat_counter[i] > 0)
-		{
-			pthread_mutex_unlock(&(philo->end_flag->mutex));
-			time_check(start, philo);
-			return ;
-		}
-		i++;
-	}
-	philo->end_flag->finish = 1;
-	pthread_mutex_unlock(&(philo->end_flag->mutex));
-}
-
-int	check_end_flag(t_endflag *end_flag)
-{
-	int	end;
-
-	end = 0;
-	pthread_mutex_lock(&(end_flag->mutex));
-	if (end_flag->finish == 1)
-		end = 1;
-	pthread_mutex_unlock(&(end_flag->mutex));
-	return (end);
-}
-
-/* Check if the philosopher still have time to be alive */
-void	time_check(struct timeval *start, t_philo *philo)
+/* Check if the philosopher is dead. If so, print the died message */
+void	check_philo_life(struct timeval *start, t_philo *philo)
 {
 	struct timeval	now;
 	long int		milliseconds;
